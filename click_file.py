44d127bbbce83d6e935932b10a4e9b5c06317e01
ajay-v-nayaka <ajay.nayak@relevancelab.com>
2020-10-13 09:39:44 +0530

    done

--- click_file.py
+++ click_file.py
@@ -1,6 +1,7 @@
 import click 
 import random
 import string
+import os
 
 def autogen(length):
     # Random string with the combination of lower and upper case
@@ -19,11 +20,23 @@ def main():
 @click.option("--name","-n",default=autogen(5),help="Stack name")
 @click.option("--compute","-c",default="t2.micro",help="Compute machine type")
 @click.option("--region","-r",default="us-west-1",help="Environment region")
-@click.option("--generate-template-only","-t")
-@click.option("--deploy","-d")
+@click.option("--generate-template-only","-t",type=bool)
+@click.option("--deploy","-d",type=bool)
 @click.option("--output","-o")
 def aws_tool(name,compute,region,generate_template_only,deploy,output):
-    
+    if(generate_template_only!=None and deploy!=None):
+        click.echo("Use either genetare template only or deploy but both cannot be used simultaniously")
+
+    else:
+        if(generate_template_only==None and deploy==True): 
+            os.system("aws cloudformation create-stack --stack-name {} --template-body file://instance.json --parameters ParameterKey=InstanceType,ParameterValue={} ".format(name,compute))
+            if(output!=None):
+                os.system("cp instance.json {}".format(output))
+        elif(generate_template_only==True and deploy==None): 
+            os.system("cat instance.json")
+            if(output!=None):
+                os.system("mv instance.json {}".format(output))
+
 
 main.add_command(aws_tool)    
 
